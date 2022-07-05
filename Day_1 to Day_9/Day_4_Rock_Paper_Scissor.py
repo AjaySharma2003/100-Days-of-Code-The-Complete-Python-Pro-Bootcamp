5ae95c4b11fa25f8e5b97afed2300c4338e4d018
Ajay Sharma <94903858+AjaySharma2003@users.noreply.github.com>
2022-07-05 05:49:02 -0500

    Add files via upload

---
+++ Day_1 to Day_9/Day_4_Rock_Paper_Scissor.py
@@ -0,0 +1,67 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+rock_paper_scissors=[rock,paper,scissors]
+user_input=int(input("What do you choose? Type 0 for Rock,1 for paper or 2 for scissors."))
+print("User input")
+if user_input<0 or user_input>=3:
+    print("You input is not available!You Lose!")
+else:
+    print(rock_paper_scissors[user_input])
+    computer_choice=random.randint(0,2)
+    print("Computer choice")
+    print(rock_paper_scissors[computer_choice])
+    win='''
+                  You Win!
+                 ___________
+                '._==_==_=_.'
+                .-\:      /-.
+               | (|:.     |) |
+                '-|:.     |-'
+                  \::.    /
+                   '::. .'
+                     ) (
+                   _.' '._
+                  `"""""""`
+                   '''
+    lose="You Lose"
+    result=" "
+    if user_input==0 and computer_choice==2:
+        result=win
+    elif user_input==0 and computer_choice==1:
+        result=lose
+    if user_input==1 and computer_choice==0:
+        result=win
+    elif user_input==1 and computer_choice==2:
+        result=lose
+    if user_input==2 and computer_choice==1:
+        result=win
+    elif user_input==2 and computer_choice==0:
+        result=lose
+    if user_input==computer_choice:
+        result="Draw!"
+    print(result)
